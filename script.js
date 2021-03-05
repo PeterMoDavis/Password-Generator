4cf53855ada95d6268bf68f8bd26d10cdbfbbdc8
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-05 18:04:53 -0500

    added alert and confirms as well as checks

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
+//============================================
+
+//PASSWORD GENERATOR!!!!!!!!!!!!!!
+
+//===========================================
+
 //Different choices
 const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
 const lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const numeric = "1234567890".split("");
 
+//hookup button
+
+let button = document.querySelector("#generate");
+
+button.addEventListener("click", () => {
+  //length of password
+  let passwordLength = prompt("How long would you like the password to be");
+  console.log(passwordLength);
+
+  //length check ---
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("the password must be at least 8 characters and no more than 128 ");
+  }
+  //=========================================================================
+
+  //confirms lowerCase
+  let lowerCaseConfirm = confirm("Would you like lowercase in your password?");
+  console.log(lowerCaseConfirm);
+  //==========================================================================
+
+  //confirms upperCase
+  let upperCaseConfirm = confirm("Would you like uppercase in your password?");
+  console.log(upperCaseConfirm);
+  // ===========================================================================
+
+  //confirms numeric
+  let numericConfirm = confirm("Would you like numerals in your password?");
+  console.log(numericConfirm);
+  //
+  if (
+    lowerCaseConfirm === false &&
+    upperCaseConfirm === false &&
+    numericConfirm === false
+  ) {
+    alert("You must pick at least one character for you password.");
+  }
+});
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-}
+//   passwordText.value = password;
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
