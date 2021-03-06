da824619ec54f88c56e4e114f1993c8ba7895bcb
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-03-06 11:07:58 -0500

    added the confirms to push the different character types

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
 const lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const numeric = "1234567890".split("");
+let passwordArray = [];
 
 //hookup button
 
@@ -22,30 +23,48 @@ button.addEventListener("click", () => {
   //length check ---
   if (passwordLength < 8 || passwordLength > 128) {
     alert("the password must be at least 8 characters and no more than 128 ");
+    return;
   }
   //=========================================================================
-
   //confirms lowerCase
   let lowerCaseConfirm = confirm("Would you like lowercase in your password?");
-  console.log(lowerCaseConfirm);
+  if (lowerCaseConfirm) {
+    passwordArray.push(...lowerCase);
+  }
   //==========================================================================
 
   //confirms upperCase
   let upperCaseConfirm = confirm("Would you like uppercase in your password?");
-  console.log(upperCaseConfirm);
+  if (upperCaseConfirm) {
+    passwordArray.push(...upperCase);
+  }
+
   // ===========================================================================
 
   //confirms numeric
   let numericConfirm = confirm("Would you like numerals in your password?");
-  console.log(numericConfirm);
-  //
+  if (numericConfirm) {
+    passwordArray.push(...numeric);
+  }
+  //=======================================================================
+  //confirms specialCharacters
+  let specialCharactersConfirm = confirm(
+    "Would you like special characters in your password?"
+  );
+  if (specialCharactersConfirm) {
+    passwordArray.push(...specialCharacters);
+  }
+
   if (
-    lowerCaseConfirm === false &&
-    upperCaseConfirm === false &&
-    numericConfirm === false
+    !lowerCaseConfirm &&
+    !upperCaseConfirm &&
+    !numericConfirm &&
+    !specialCharactersConfirm
   ) {
     alert("You must pick at least one character for you password.");
+    return;
   }
+  console.log(passwordArray);
 });
 
 // Assignment Code
